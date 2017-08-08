c6203e15763f4cc07f2dd89d9b05cb3de658eb0a
Alex Bazlinton <abazlinton@gmail.com>
2017-08-08 13:37:50 +0100

    detect which div clicked update model

--- test.elm
+++ test.elm
@@ -1,6 +1,6 @@
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick)
 import Array exposing (..)
 
 main =
@@ -10,22 +10,34 @@ main =
     , update = update 
     }
 
-cellToDiv : Int -> String -> Html msg
-cellToDiv index cell =
-  div [id ( toString index )][text cell]
+type Msg = Take Int
 
-type Msg = Dummy String
+cellToDiv : Int -> String -> Html Msg
+cellToDiv index cell =
+  div [id ( toString index ), class "cell", onClick (Take index)][text cell]
 
 type alias Model =
-  { board : Array String }
+  { board : Array String 
+  , turn : String
+  }
 
 model : Model
-model = {board = Array.repeat 9 ""}
+model = 
+  { board = Array.repeat 9 "O"
+  , turn = "X"
+  }
 
 
-view : Model -> Html msg
+view : Model -> Html Msg
 view model =
-      node "div" [id "board"] ( toList ( Array.indexedMap cellToDiv model.board ) )
+  div [][
+  node "div" [id "board"] ( toList ( Array.indexedMap cellToDiv model.board ) )
+  , div [][text model.turn]
+  ]
     
 update : Msg -> Model -> Model
-update msg model = model
+update msg model =
+  case msg of
+      Take index ->
+        {model | turn = toString index}
+  
